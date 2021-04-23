77ff2c074e9c2a56286244ecb03e08b4cca735ea
Ema Cortez <emanuelcortez32@gmail.com>
2021-04-22 21:05:18 -0300

    Initial commit

---
+++ src/components/ToDoList/ToDoList.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export class ToDoList extends React.Component {
+    constructor(props){
+        super(props)
+        this.props = props
+        this.state = {
+            inputValue: "",
+            tasks : ["Aprender React","Aprender WebPack","Aprender Babel"]
+        }
+    }
+
+    addTask(task){
+        this.setState({
+          tasks: [...this.state.tasks, task]
+        })
+    }
+    
+    handleInput(event){
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
+
+    render(){
+        return (
+            <>
+                <ul>
+                    { this.state.tasks.map((task, index) => { return <li key={index}>{task}</li>}) }
+                </ul>
+                <input value={this.state.inputValue} onChange={(e) => this.handleInput(e)} type="text" /><button onClick={() => this.addTask(this.state.inputValue)}>Agregar</button>
+            </>
+        )
+    }
+}
